a2e9896dedc4ce2145c11865192e401647bc344c
Heesung jang <heesungj7@gmail.com>
2021-06-23 01:07:53 +0900

    20210623 - Homecontainer + Loader

--- nomflix/src/Routes/TV/TvPresenter.js
+++ nomflix/src/Routes/TV/TvPresenter.js
@@ -1,13 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "../../Components/Section";
+import Loader from "../../Components/Loader";
 
-const TvPresenter = ({ topRated, popular, airingTody, loading, error }) => null;
+const Container = styled.div`
+    padding: 0px 10px;
+`;
+
+const TvPresenter = ({ topRated, popular, airingToday, loading, error }) =>
+    loading ? (
+        <Loader />
+    ) : (
+        <Container>
+            {topRated && topRated.length > 0 && (
+                <Section title="Top Rated">
+                    {topRated.map((show) => show.name)}
+                </Section>
+            )}
+            {popular && popular.length > 0 && (
+                <Section title="Popular">
+                    {popular.map((show) => show.name)}
+                </Section>
+            )}
+
+            {airingToday && airingToday.length > 0 && (
+                <Section title="Airing Today">
+                    {airingToday.map((show) => show.name)}
+                </Section>
+            )}
+        </Container>
+    );
 
 TvPresenter.propTypes = {
     topRated: PropTypes.array,
     popular: PropTypes.array,
-    airingTody: PropTypes.array,
+    airingToday: PropTypes.array,
     loading: PropTypes.bool.isRequired,
     error: PropTypes.string,
 };
